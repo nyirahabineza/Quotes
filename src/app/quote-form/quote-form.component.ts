d25ae7aeceb77437bf4caa0ccad9e9a2f1a8d1b2
nyirahabineza <habcecile@gmail.com>
2019-01-29 15:59:40 +0200

    adding selector

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,7 +1,7 @@
-import { Component,OnInit ,Output,  } from '@angular/core';
+import { Component,OnInit ,Output, EventEmitter } from '@angular/core';
 import {Quote} from '../quotes';
 @Component({
-  selector: 'app-quote-form',
+  : 'app-quote-form',
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
